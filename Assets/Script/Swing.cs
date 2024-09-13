0018ab2ca95b4fd35366a5fdf71915355e817591
Calipur <mycroftn@163.com>
2024-09-13 20:57:56 +0300

    (+)rotation objects
    (+)rotation objects

---
+++ Assets/Script/Swing.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+    private bool turnRight;
+    private bool turnLeft;
+    private float turnDegree = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.transform.localRotation.z == 0)
+        {
+            turnRight = true;
+            turnLeft = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Right();
+        Left();
+    }
+
+    private void Right()
+    {
+        if (turnRight == true)
+        {
+            if (turnDegree < 2.0f)
+            {
+                turnDegree += Time.deltaTime;
+                this.transform.localEulerAngles = new Vector3(0, 0, turnDegree);
+            }
+            else
+            {
+                turnRight = false;
+                turnLeft = true;
+            }
+        }
+    }
+    private void Left()
+    {
+        if (turnLeft == true)
+        {
+            if (turnDegree > -2.0f)
+            {
+                turnDegree -= Time.deltaTime;
+                this.transform.localEulerAngles = new Vector3(0, 0, turnDegree);
+            }
+            else
+            {
+                turnRight = true;
+                turnLeft = false;
+            }
+        }
+    }
+
+
+}
